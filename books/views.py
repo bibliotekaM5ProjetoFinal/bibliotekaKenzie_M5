3a5a828a04969d64a5bc0dd5d97085357db22661
Lucas Magalhães <lucoder618@gmail.com>
2023-03-06 11:15:09 -0300

    feat: books views/serializers

--- books/views.py
+++ books/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
 from rest_framework import generics
+from .models import Book, BookCopy
+from .serializers import BookSerializer
 
 # Create your views here.
 
 
-class BookView(generics.CreateAPIView):
-    ...
-
+class BookView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 
 class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
-    ...
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    lookup_url_kwarg = "pk"
 
 
 class BookCopyView(generics.CreateAPIView):
