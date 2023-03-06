e522cc2b7208c17e4285f5ed8900f6ef2c75a1a5
Lucas Magalhães <lucoder618@gmail.com>
2023-03-06 12:52:32 -0300

    feat: BookCopies views/serializers created

--- books/serializers.py
+++ books/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
-from .models import Book
-
+from .models import Book, BookCopy
+from rest_framework.response import Response
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
@@ -12,4 +12,19 @@ class BookSerializer(serializers.ModelSerializer):
         ]
         extra_kwargs = {
             "id" : {"read_only" : True}
+        }
+
+class BookCopySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = BookCopy
+        fields = [
+            "id",
+            "book",
+            "added_on"
+        ]
+        extra_kwargs = {
+            "id" : {"read_only" : True},
+            "book" : {"read_only" : True},
+            "added_on" : {"read_only" : True}
         }
--- books/views.py
+++ books/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from rest_framework import generics
+from django.shortcuts import get_object_or_404
+from rest_framework import generics, status
+from rest_framework.views import Response
 from .models import Book, BookCopy
-from .serializers import BookSerializer
+from .serializers import BookSerializer, BookCopySerializer
 
 # Create your views here.
 
@@ -16,9 +18,39 @@ class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
     lookup_url_kwarg = "pk"
 
 
-class BookCopyView(generics.CreateAPIView):
-    ...
+class BookCopyView(generics.ListCreateAPIView):
+    queryset = BookCopy.objects.all()
+    serializer_class = BookCopySerializer
+    lookup_url_kwarg = "book_pk"
 
+    def list(self, request, *args, **kwargs):
+        book = get_object_or_404(Book, pk=self.kwargs.get("book_pk"))
+        queryset = BookCopy.objects.filter(book = book)
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+    
+    def perform_create(self, serializer):
+        book = get_object_or_404(Book, pk=self.kwargs.get("book_pk"))
+        serializer.save(book = book)
 
-class BookCopyDetailView(generics.RetrieveUpdateDestroyAPIView):
-    ...
+class BookCopyDetailView(generics.RetrieveDestroyAPIView):
+    queryset = BookCopy.objects.all()
+    serializer_class = BookCopySerializer
+    lookup_url_kwarg = "book_pk"
+
+    def retrieve(self, request, *args, **kwargs):
+        book = get_object_or_404(Book, pk=self.kwargs.get("book_pk"))
+        copy = get_object_or_404(BookCopy, pk=self.kwargs.get("copy_pk"), book=book)
+        serializer = self.get_serializer(copy)
+        return Response(serializer.data)
+    
+    def destroy(self, request, *args, **kwargs):
+        book = get_object_or_404(Book, pk=self.kwargs.get("book_pk"))
+        copy = get_object_or_404(BookCopy, pk=self.kwargs.get("copy_pk"), book=book)
+        self.perform_destroy(copy)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
