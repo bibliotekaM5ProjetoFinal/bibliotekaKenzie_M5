44d1650aa6a4ae1c10866afe99ee40cd3e849e8e
Yokorosan <etherhiroaki@hotmail.com>
2023-03-06 21:34:49 -0300

    follow almost done

--- books/serializers.py
+++ books/serializers.py
@@ -3,10 +3,18 @@ from .models import Book, BookCopy
 from rest_framework.response import Response
 
 
+class FollowedListingField(serializers.RelatedField):
+    def to_representation(self, value):
+        follow = {"email": value.user.email, "since": value.start_following}
+        return follow
+
+
 class BookSerializer(serializers.ModelSerializer):
+    followed_by = FollowedListingField(many=True, read_only=True)
+
     class Meta:
         model = Book
-        fields = ["id", "title", "author", "synopsis", "copies"]
+        fields = ["id", "title", "author", "synopsis", "copies", "followed_by"]
         depth = 1
         extra_kwargs = {"id": {"read_only": True}}
 
--- follows/views.py
+++ follows/views.py
@@ -1,12 +1,52 @@
-from django.shortcuts import render
-from rest_framework import generics
+from django.shortcuts import get_list_or_404, get_object_or_404, render
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework import generics, status
+from books.models import Book
+from users.models import User
+from users.permissions import AuthOnlyPermission, StaffOrOwnerPermission, TestPermission
+from .models import Follow
+from rest_framework.views import Response
+
+
+from follows.serializers import FollowSerializer
 
 # Create your views here.
 
 
 class FollowView(generics.CreateAPIView):
-    ...
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [AuthOnlyPermission]
+
+    queryset = Follow.objects.all()
+    serializer_class = FollowSerializer
+
+    def perform_create(self, serializer):
+        user = get_object_or_404(User, id=self.request.user.id)
+        book = get_object_or_404(Book, id=self.request.data.get("bookId"))
+        serializer.save(user=user, book=book)
 
 
 class FollowDetailView(generics.RetrieveDestroyAPIView):
-    ...
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [AuthOnlyPermission, StaffOrOwnerPermission]
+
+    queryset = Follow.objects.all()
+    serializer_class = FollowSerializer
+    lookup_url_kwarg = "pk"
+
+    def retrieve(self, request, *args, **kwargs):
+        follow = get_list_or_404(Follow, user=self.kwargs.get("pk"))
+        user = get_object_or_404(User, id=self.kwargs.get("pk"))
+
+        self.check_object_permissions(request, user)
+        print(self.check_object_permissions(request, user))
+        serializer = self.get_serializer(follow, many=True)
+        return Response(serializer.data)
+
+    def destroy(self, request, *args, **kwargs):
+        follow = get_object_or_404(Follow, id=self.kwargs.get("pk"))
+        user = get_object_or_404(User, following=self.kwargs.get("pk"))
+        self.check_object_permissions(request, user)
+
+        self.perform_destroy(follow)
+        return Response(status=status.HTTP_204_NO_CONTENT)
