d6025bbd96ad85360531072869cea25e6715a179
Yokorosan <etherhiroaki@hotmail.com>
2023-03-09 16:28:26 -0300

    setting up the drf documentation for API

--- follows/serializers.py
+++ follows/serializers.py
@@ -1,15 +1,19 @@
 from rest_framework import serializers
 from rest_framework.validators import UniqueTogetherValidator
 from .models import Follow
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import extend_schema_field
 
 
 class FollowSerializer(serializers.ModelSerializer):
     user = serializers.SerializerMethodField()
     book = serializers.SerializerMethodField()
 
+    @extend_schema_field(OpenApiTypes.EMAIL)
     def get_user(self, obj):
         return obj.user.email
 
+    @extend_schema_field(OpenApiTypes.STR)
     def get_book(self, obj):
         return obj.book.title
 
